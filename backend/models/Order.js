a9ecfafcbcbae75e9c6f6e87560b2bf39ac3fea3
chandrashekar <chandrashekar8pg@gmail.com>
2025-09-13 01:14:09 +0530

    Update Order.js model

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -11,7 +11,7 @@ const OrderSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   items: [OrderItemSchema],
   total: { type: Number, required: true },
-  status: { type: String, default: 'placed' }, // placed, shipped, delivered etc.
+  status: { type: String, default: 'placed' }, 
   placedAt: { type: Date, default: Date.now }
 });
 
